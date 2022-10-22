a0972900b051ccfed91de3efc8ed0296b6555cd8
Mathew Chandler <Cascabelrock@gmail.com>
2022-10-21 19:22:34 -0700

    getting started

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// 1. create button
+startButtonEl = document.getElementById("start-btn")
+startButtonEl.addEventListener("click", startQuiz) 
+function startQuiz() {
+    console.log("Hello")
+}
+// 2. start timer 
+
+// 3. a question appear to user with 4 (text)
+
+// 4. timer counts down from 5 minutes
+
+// 5. the quiz ends when the timer gets to 0
+
