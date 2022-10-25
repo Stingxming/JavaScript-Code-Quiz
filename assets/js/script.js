466c74906bd1993901fc5f1c379433a65be0788b
Mathew Chandler <Cascabelrock@gmail.com>
2022-10-24 17:07:43 -0700

    adding html elements and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,33 @@ function startQuiz() {
 }
 // 2. start timer 
 
+var secondsLeft = 66666;
+var startQuiz
+setTime()
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    counter.textContent = secondsLeft + " left on the clock.";
+
+    if(secondsLeft === 0) {
+
+      clearInterval(timerInterval);
+
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
 // 3. a question appear to user with 4 (text)
+
+var questions = [
+    {
+        prompt: "",
+        answer: ""
+    },
+]
+var score = 0;
 
 // 4. timer counts down from 5 minutes
 
