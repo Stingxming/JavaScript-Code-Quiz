6d3ccdbee6fc1ab73acd2372054e47240740f2a5
Mathew Chandler <Cascabelrock@gmail.com>
2022-10-25 21:36:24 -0700

    adding javaScript to repo

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # JavaScript-Code-Quiz
 
-I have to submit this incomplete because I'm still working on it. Eventually I will have a multiple choice quiz built in HTML with interactive logic in javaScript. My next submission of this assignment will be much further along. I spent hours finishing up with week 4 mini project instead of working on this challenge because I felt like I needed the practice and to try to gain a better understanding of the challenge.
+I have to submit this incomplete because I'm still working on it. Eventually I will have a multiple choice quiz built in HTML with interactive logic in javaScript. My next submission of this assignment will be much further along. I spent hours finishing up with week 4 mini project instead of working on this challenge because I felt like I needed the practice and to try to gain a better understanding of the challenge. I have also included a link to my repo to show progress on the mini project so that you can see I am working towards my goal!
+
+## Deployed-Page: https://github.com/Stingxming/JavaScript-Code-Quiz
+
+### Mini-Project: https://stingxming.github.io/Week4-mini-project/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,50 @@
 // 1. create button
-startButtonEl = document.getElementById("start-btn")
-startButtonEl.addEventListener("click", startQuiz) 
+startButtonEl = document.getElementById("playgame");
+startButtonEl.addEventListener("click", startQuiz);
 function startQuiz() {
-    console.log("Hello")
+  console.log("Hello");
 }
-// 2. start timer 
+// 2. start timer
 
-var secondsLeft = 66666;
-var startQuiz
-setTime()
+var secondsLeft = 0;
+var startQuiz;
+setTime();
 function setTime() {
-  var timerInterval = setInterval(function() {
+  var timerInterval = setInterval(function () {
     secondsLeft--;
     counter.textContent = secondsLeft + " left on the clock.";
 
-    if(secondsLeft === 0) {
-
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
 
       sendMessage();
     }
-
   }, 1000);
 }
 
 // 3. a question appear to user with 4 (text)
 
 var questions = [
-    {
-        prompt: "",
-        answer: ""
-    },
-]
+  {
+    prompt: "What is the color of the sky?",
+    answer: "Blue",
+    choices: ["Blue", "Red", "Yellow", "Green"],
+  },
+];
 var score = 0;
+
+function initPrompt() {
+  for (var i = 0; i < questions.length; i++) {
+    var response = window.prompt(questions[i].prompt);
+    if (response == questions[i].answer) {
+      score++;
+      alert("BOOYA!");
+    } else {
+      alert("NOPE!");
+    }
+  }
+}
 
 // 4. timer counts down from 5 minutes
 
 // 5. the quiz ends when the timer gets to 0
-
